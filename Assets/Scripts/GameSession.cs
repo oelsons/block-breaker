3cc789140949a66b0633295eb22feef2823b1b1f
oelsons <oelsonseg@hotmail.com>
2019-05-26 02:27:10 -0300

    Finished game

--- Assets/Scripts/GameSession.cs
+++ Assets/Scripts/GameSession.cs
@@ -10,6 +10,7 @@ public class GameSession : MonoBehaviour
     [Range(0.1f, 10f)] [SerializeField] float gameSpeed = 1f;
     [SerializeField] int pointsPerBlockDestroyed = 83;
     [SerializeField] TextMeshProUGUI scoreText;
+    [SerializeField] bool isAutoPlayEnabled;
 
     // state variables
     [SerializeField] int currentScore = 0;
@@ -47,6 +48,11 @@ public class GameSession : MonoBehaviour
     public void ResetGame()
     {
         Destroy(gameObject);
+    }
+
+    public bool IsAutoPlayEnabled()
+    {
+        return isAutoPlayEnabled;
     }
 
 }
