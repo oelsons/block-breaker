71ce0f8d7064e0275fa0c6805e3403235881df3b
oelsons <oelsonseg@hotmail.com>
2019-05-25 21:42:57 -0300

    First Commit

---
+++ Assets/Scripts/Level.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level : MonoBehaviour
+{
+    //parameters
+    [SerializeField] int breakableBlocks; // Serialized for debugging purposes
+
+    // Cached reference
+    SceneLoader sceneLoader;
+
+    public void CountBreakableBlocks()
+    {
+        breakableBlocks++;
+        sceneLoader = FindObjectOfType<SceneLoader>();
+    }
+
+    public void BlockDestroyed()
+    {
+        breakableBlocks--;
+        if(breakableBlocks <= 0)
+        {
+            sceneLoader.LoadNextScene();
+        }
+    }
+}
